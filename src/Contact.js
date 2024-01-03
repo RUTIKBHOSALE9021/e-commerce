1df14fe9bdc4385597db2ed19c8f3d2169313f49
RUTIKBHOSALE9021 <rutikbhosale7218@gmail.com>
2024-01-03 23:04:42 +0530

    First Commit

--- src/Contact.js
+++ src/Contact.js
@@ -4,23 +4,18 @@ const Contact = () => {
   const Wrapper = styled.section`
     padding: 9rem 0 5rem 0;
     text-align: center;
-
     .container {
       margin-top: 6rem;
-
       .contact-form {
         max-width: 50rem;
         margin: auto;
-
         .contact-inputs {
           display: flex;
           flex-direction: column;
           gap: 3rem;
-
           input[type="submit"] {
             cursor: pointer;
             transition: all 0.2s;
-
             &:hover {
               background-color: ${({ theme }) => theme.colors.white};
               border: 1px solid ${({ theme }) => theme.colors.btn};
@@ -33,7 +28,55 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h4 className="common-heading" style={{color:"#663366"}}>Hey, I am Rutik. Thank you for visiting our website. We'd love to hear from you!</h4>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30073.563835794055!2d74.1874978121!3d19.576124230959355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdd01d0a9ebab79%3A0x46dd52016aa8be88!2sSangamner%2C%20Maharashtra%20422605!5e0!3m2!1sen!2sin!4v1683091045912!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xdovbkze"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
