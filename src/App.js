7e8ef5155f1dc57921175f421e2e7d8ee802521b
RUTIKBHOSALE9021 <rutikbhosale7218@gmail.com>
2024-01-03 23:00:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
-};
+  return (
+    <div>
+      <h1>This is e-commerce website</h1>
+    </div>
+  )
+}
 
-export default App;
+export default App
